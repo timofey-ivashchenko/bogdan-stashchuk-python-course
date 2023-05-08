b410e04f9bf0c9bcaf57becff33e28af2d47b47b
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-08 10:41:57 +0300

    Реализована функция представления по умолчанию приложения shop.

--- base/urls.py
+++ base/urls.py
@@ -26,8 +26,9 @@ Including another URL configuration
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls')),
 ]
