19833c23250825103a0dcf4060bf824af225fe39
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-08 13:40:57 +0300

    Косметика.

--- shop/models.py
+++ shop/models.py
@@ -3,14 +3,22 @@ from django.utils import timezone
 
 
 class Category(models.Model):
+
     title = models.CharField(max_length=255)
+
     created_at = models.DateTimeField(default=timezone.now)
 
 
 class Course(models.Model):
+
     title = models.CharField(max_length=255)
+
     price = models.FloatField()
+
     student_quantity = models.IntegerField()
+
     review_quantity = models.IntegerField()
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(default=timezone.now)
