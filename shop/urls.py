bed4149e90fb40a8d03f23d3a81a9345dd953749
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-14 03:36:15 +0300

    Реализована страница отдельного курса.

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:course_id>', views.single_course, name='single_course'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 
 
 def index(request):
+    courses = Course.objects.all()
     return render(
         request=request,
         template_name='courses.html',
-        context={'courses': Course.objects.all()})
+        context={'courses': courses}
+    )
+
+
+def single_course(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    return render(
+        request=request,
+        template_name='single_course.html',
+        context={'course': course}
+    )
