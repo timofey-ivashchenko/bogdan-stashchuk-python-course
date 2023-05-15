42b6c756aa1fa07c86f720f9eb6d5e92a8a83cf8
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-16 02:25:56 +0300

    Перенос магазина на главную страницу.

--- base/urls.py
+++ base/urls.py
@@ -29,7 +29,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('shop.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('shop/', include('shop.urls'))
+    path('api/', include('api.urls'))
 ]
