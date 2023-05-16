6d44a54f4ea6fc14a54bcbf330eb67d05affbe30
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-16 21:57:26 +0300

    Настройка редактирования моделей в панели администрирования.

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,55 @@
-from . import models
+from .models import Category, Course
 from django.contrib import admin
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+
+class CourseInline(admin.TabularInline):
+
+    extra = 0
+
+    fields = (
+        'title',
+        'category',
+        'price',
+        'student_quantity',
+        'review_quantity'
+    )
+
+    model = Course
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    fields = ['title']
+
+    inlines = [CourseInline]
+
+    list_display = ('id', 'title', 'created_at')
+
+    list_display_links = ['title']
+
+
+class CourseAdmin(admin.ModelAdmin):
+
+    fields = (
+        'title',
+        'category',
+        'price',
+        'student_quantity',
+        'review_quantity'
+    )
+
+    list_display = (
+        'id',
+        'title',
+        'category',
+        'price',
+        'student_quantity',
+        'review_quantity',
+        'created_at'
+    )
+
+    list_display_links = ['title']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course, CourseAdmin)
