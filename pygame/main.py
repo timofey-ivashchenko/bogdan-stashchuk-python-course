3d5f679244650b4dbf3190fdce939326f4338c5e
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-18 23:15:40 +0300

    Прорисовка прямоугольника в центре окна.

--- pygame/main.py
+++ pygame/main.py
@@ -1,23 +1,40 @@
 import sys
-from random import randint
 
 import pygame
 
-# Initialize the game.
+# Инициализация игры.
 pygame.init()
-# Set the window caption.
-pygame.display.set_caption('My PyGame')
-# Set the window size.
-screen = pygame.display.set_mode((800, 600))
 
-# Handle the window events.
+# Установка заголовка окна.
+pygame.display.set_caption('My PyGame')
+
+# Установка размера окна.
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Определение размера и вычисление координат прямоугольника.
+rect_width = 100
+rect_height = 200
+rect_x = (screen_width - rect_width) / 2
+rect_y = (screen_height - rect_height) / 2
+rect = (rect_x, rect_y, rect_width, rect_height)
+
+# Определение цветов окна и прямоугольника.
+screen_color = (2, 84, 100)
+rect_color = (232, 170, 66)
+
+# Обработка событий окна.
 while True:
     for event in pygame.event.get():
-        # Print the event info.
+        # Вывод в терминал информации о событии.
         print(event)
-        # Exit on the QUIT event.
+        # Завершение работы программы по событию QUIT.
         if event.type == pygame.QUIT:
             sys.exit()
-        # Set the background color of the window to a random color.
-        screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+        # Заливка фона окна.
+        screen.fill(screen_color)
+        # Прорисовка прямоугольника в центре окна.
+        pygame.draw.rect(screen, rect_color, rect)
+        # Обновление экрана.
         pygame.display.update()
