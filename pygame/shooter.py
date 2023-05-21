c3fe68084d8e78b7c56b53090fb3c0764e0a8b8c
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-21 10:32:12 +0300

    Размещение корабля внизу по центру экрана.

--- pygame/shooter.py
+++ pygame/shooter.py
@@ -2,22 +2,24 @@ import sys
 
 import pygame
 
-# Инициализация игры.
 pygame.init()
-
-# Установка заголовка окна.
 pygame.display.set_caption('Pygame Shooter')
 
-# Установка размера окна.
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Обработка событий окна.
+FIGHTER_IMAGE = pygame.image.load('images/fighter.png')
+FIGHTER_WIDTH, FIGHTER_HEIGHT = FIGHTER_IMAGE.get_size()
+fighter_x = (SCREEN_WIDTH - FIGHTER_WIDTH) / 2
+FIGHTER_Y = SCREEN_HEIGHT - FIGHTER_HEIGHT
+
 while True:
     for event in pygame.event.get():
-        # Вывод в терминал информации о событии.
         print(event)
-        # Завершение работы программы по событию QUIT.
+
         if event.type == pygame.QUIT:
             sys.exit()
+
+    SCREEN.fill((32, 52, 71))
+    SCREEN.blit(FIGHTER_IMAGE, (fighter_x, FIGHTER_Y))
+    pygame.display.update()
