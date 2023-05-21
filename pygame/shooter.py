3536de130d8be4e584eb514571afd9080b6f979e
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2023-05-21 12:28:40 +0300

    Исправлены комментарии.

--- pygame/shooter.py
+++ pygame/shooter.py
@@ -22,21 +22,24 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
+            # Start moving the ship to the left by pressing down the left arrow key.
             if event.key == pygame.K_LEFT:
                 moving_left = True
+            # Start moving the ship to the right by pressing down the right arrow key.
             elif event.key == pygame.K_RIGHT:
                 moving_right = True
+        # Stop moving the ship by pressing up the left or right arrow keys.
         elif event.type == pygame.KEYUP:
             moving_left = False
             moving_right = False
 
-    # Move the ship to the left by clicking on the left arrow.
+    # Move the ship to the left while pressing the left arrow key.
     if moving_left:
         fighter_x -= STEP
         if fighter_x < 0:
             fighter_x = 0
 
-    # Move the ship to the right when clicking on the right arrow.
+    # Move the ship to the right while pressing the right arrow key.
     if moving_right:
         fighter_x += STEP
         if fighter_x + FIGHTER_WIDTH > SCREEN_WIDTH:
